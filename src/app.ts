de7dfdcba5fa1a01c2797ae1120e36715ce0ae1e
Mike Coon <mac718@gmail.com>
2023-01-13 14:08:04 -0800

    remove dummy routes from app

--- src/app.ts
+++ src/app.ts
@@ -32,17 +32,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/tasks", tasks);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("herr0");
-});
-
-app.post("/thing", async (req: Request, res: Response) => {
-  console.log("butts");
-  const task = await myDataSource.getRepository(Task).create(req.body);
-  const results = await myDataSource.getRepository(Task).save(task);
-  res.send(results);
-});
-
 app.listen(PORT, (): void => {
   console.log(`Listening on port: ${PORT}`);
 });
