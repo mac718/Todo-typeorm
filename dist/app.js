df639c5dc85c48f4bf64b27e2376eb987abfd87b
Mike Coon <mac718@gmail.com>
2023-01-13 18:14:25 -0800

    specify outDir in tsconfig - change paths in config/development and test and point npm start at dist/app.js

--- dist/app.js
+++ dist/app.js
@@ -7,12 +7,6 @@ var express_1 = __importDefault(require("express"));
 var tasks_1 = require("./routes/tasks");
 var cors_1 = __importDefault(require("cors"));
 var taskRepository_1 = require("./repositories/taskRepository");
-//import { DataSource, DataSourceOptions } from "typeorm";
-//let db: DataSourceOptions = config.get("database");
-// db = JSON.parse(JSON.stringify(db));
-// console.log("db", db);
-//const myDataSource = new DataSource(db);
-//console.log("db", myDataSource);
 taskRepository_1.myDataSource
     .initialize()
     .then(function () {
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,7 @@
 import express from "express";
-import { Request, Response } from "express";
-//import { myDataSource } from "./app-data-source";
-import { Task } from "./entity/task.entity";
 import { tasks } from "./routes/tasks";
 import cors from "cors";
-import config from "config";
 import { myDataSource } from "./repositories/taskRepository";
-//import { DataSource, DataSourceOptions } from "typeorm";
-
-//let db: DataSourceOptions = config.get("database");
-// db = JSON.parse(JSON.stringify(db));
-// console.log("db", db);
-
-//const myDataSource = new DataSource(db);
-
-//console.log("db", myDataSource);
 
 myDataSource
   .initialize()
