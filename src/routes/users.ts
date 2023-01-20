59a3204bec51c5b50fccc588dccdf146ce6aa866
Mike Coon <mac718@gmail.com>
2023-01-19 21:40:48 -0800

    add validations to user router

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,23 @@
 import { login, register } from "../controllers/usersController";
 import express from "express";
+import { body } from "express-validator";
+
 const router = express.Router();
 
 export const users = router;
 
-users.route("/").post(register);
-users.route("/login").post(login);
+users
+  .route("/")
+  .post(
+    body("name").notEmpty().trim().escape(),
+    body("email").trim().escape().isEmail(),
+    body("password").isLength({ min: 5 }),
+    register
+  );
+users
+  .route("/login")
+  .post(
+    body("email").trim().escape().isEmail(),
+    body("password").isLength({ min: 5 }),
+    login
+  );
