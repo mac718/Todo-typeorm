c392049b6ed56f964b05d08c9a40414415eb59eb
Mike Coon <mac718@gmail.com>
2023-01-20 09:22:18 -0800

    remove dist and .env from tracked files

--- dist/routes/users.js
+++ dist/routes/users.js
@@ -6,7 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.users = void 0;
 var usersController_1 = require("../controllers/usersController");
 var express_1 = __importDefault(require("express"));
+var express_validator_1 = require("express-validator");
 var router = express_1.default.Router();
 exports.users = router;
-exports.users.route("/").post(usersController_1.register);
-exports.users.route("/login").post(usersController_1.login);
+exports.users
+    .route("/")
+    .post((0, express_validator_1.body)("name").notEmpty().trim().escape(), (0, express_validator_1.body)("email").trim().escape().isEmail(), (0, express_validator_1.body)("password").isLength({ min: 5 }), usersController_1.register);
+exports.users
+    .route("/login")
+    .post((0, express_validator_1.body)("email").trim().escape().isEmail(), (0, express_validator_1.body)("password").isLength({ min: 5 }), usersController_1.login);
